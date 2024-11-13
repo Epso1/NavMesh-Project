771dcf0e44c3e69bd9ba3214e19fa69b63234f6e
Epso1 <88671456+Epso1@users.noreply.github.com>
2024-11-13 20:42:30 +0100

    Updated

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,20 +4,24 @@ using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.AI;
 
-
-
 public class Enemy : MonoBehaviour
 {
     public enum EnemyState { Patrol, Chase, Attack }
-    public Transform target;
-    NavMeshAgent agent;
-    EnemyState currentState;
-    bool facingRight = false;
-    Vector2 lastPosition = Vector2.zero;
-    public Transform patrolSpot1;
-    public Transform patrolSpot2;
-    Transform currentPatrolSpot;
-   
+    private NavMeshAgent agent;
+    private EnemyState currentState;
+    private bool facingRight = false;
+    private Vector2 lastPosition = Vector2.zero;
+    private Transform currentPatrolSpot;
+    public bool playerDetected;
+    public bool wallDetected;
+    [SerializeField] private Transform target;
+    [SerializeField] private float changeDirectionDistance = 0.9f;
+    [SerializeField] private Transform patrolSpot1;
+    [SerializeField] private Transform patrolSpot2;
+    [SerializeField] private float rayDistance;
+    [SerializeField] private LayerMask enemyLayer;
+    private Vector2 raycastDirection;
+
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
@@ -56,6 +60,22 @@ public class Enemy : MonoBehaviour
         }
 
         lastPosition = transform.position;
+
+        //if (!wallDetected && playerDetected)
+        //{
+        //    Debug.Log("Changing state to chase");
+        //    currentState = EnemyState.Chase;
+        //}
+        //else if (wallDetected)
+        //{
+        //    Debug.Log("Changing state to patrol");
+        //    currentState = EnemyState.Patrol;
+        //}
+
+        // Dirección hacia el jugador
+        raycastDirection = ((Vector2)target.position - lastPosition).normalized;
+
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, raycastDirection, rayDistance, ~enemyLayer);        
     }
 
     void ChasePlayer()
@@ -67,7 +87,7 @@ public class Enemy : MonoBehaviour
     {
         agent.SetDestination(currentPatrolSpot.position);
 
-        if (Vector3.Distance(transform.position, currentPatrolSpot.position) <= 0f)
+        if (Vector3.Distance(transform.position, currentPatrolSpot.position) <= 0.9f)
         {
             if (currentPatrolSpot == patrolSpot1)
             {
@@ -79,5 +99,12 @@ public class Enemy : MonoBehaviour
             }
         } 
        
+    }
+
+    void OnDrawGizmos()
+    {
+        // Dibuja el gizmo del raycast
+        Gizmos.color = Color.red;        
+        Gizmos.DrawLine(transform.position, transform.position + (Vector3)raycastDirection * rayDistance);    
     }
 }
